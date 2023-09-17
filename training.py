2ba84a845f63d0333b56ca6c44ff5327a63645df
Yves Velasquez <vyves@csu.fullerton.edu>
2023-09-16 23:05:44 -0700

    Train test split

--- training.py
+++ training.py
@@ -1,36 +1,38 @@
-import random
-import pandas as pd
 import tensorflow as tf
-
+import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 # SETUP
 dataset = pd.read_csv('Dataset/insparation.csv')
-model = tf.keras.models.Sequential()
-
-dataset.drop('Category', inplace=True, axis=1)
 dataset.drop('Image-link', inplace=True, axis=1)
 dataset.drop('Quote-url', inplace=True, axis=1)
 dataset.drop('Unnamed: 0', inplace=True, axis=1)
 
+x = dataset['Category']
+y = dataset['Quote']
 
-x = dataset.drop(columns=['Quote'])
-y = dataset["Quote"]
+# TRAIN TEST SPLIT
+x_train, x_test,y_train, y_test = train_test_split(x,y, 
+                                   random_state=104,
+                                   test_size=0.25,
+                                   shuffle=True)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2) # 20% of the data
+# X TRAIN
+print('X TRAIN : ')
+print(x_train.head())
+
+# X TEST
+print('X TEST : ')
+print(x_test.head())
+
+# Y TRAIN
+print('Y TRAIN : ')
+print(y_train.head())
+
+# Y TEST
+print('Y TEST : ')
+print(y_test.head())
 
 
-
-# LAYERS
-model.add(tf.keras.layers.Dense(256, input_shape=x_train.shape, activation='sigmoid'))
-model.add(tf.keras.layers.Dense(256, activation='sigmoid'))
-
-# OUTPUT
-model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
-
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-x_tensor = tf.convert_to_tensor(x_train, dtype=tf.string)
-y_tensor = tf.convert_to_tensor(y_train, dtype=tf.string)
-
-model.fit(x_tensor, y_tensor, epochs=10)
+print(dataset.head())
