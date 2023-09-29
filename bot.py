9b316e1ad7efd3628abbeec50961b30e67d77f5a
Yves Velasquez <vyves@csu.fullerton.edu>
2023-09-29 14:09:42 -0700

    Small changes

--- bot.py
+++ bot.py
@@ -2,20 +2,26 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
+from hourly_motivation import generate_image, generate_quote, load_image
+
 
 # SETUP
 load_dotenv()
 DISCORD_KEY = os.getenv("DISCORD_KEY")
-bot = discord.Client(intents=discord.Intents.default())
- 
+client = discord.Client(intents=discord.Intents.default())
 
-@bot.event
+
+quote = generate_quote("Generate a motivational quote, without quoting anyone from history.")
+background_img = generate_image("Beautiful Nature Background")
+
+
+
+@client.event
 async def on_ready():
-    print("READY")
+    print(f'Logged in as {client.user.name} (ID: {client.user.id})')
+    print('---------------')
+    channel = client.get_channel(1157410144334921760)
+    if channel:
+        await channel.send(f'{load_image(background_img,quote)}')
 
-@bot.event
-async def on_message(message):
-    await message.channel.send("Hello, world!")
-bot.run(DISCORD_KEY)
-
-## POSTPONED
+client.run(DISCORD_KEY)
